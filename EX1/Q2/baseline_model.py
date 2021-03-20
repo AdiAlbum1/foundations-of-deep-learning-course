6b1ec2758fc1611979471be59d67b124ed21c920
Adi <adi.album1@gmail.com>
2021-03-20 16:30:57 +0200

    update baseline_model for hyperparamter grid search

--- EX1/Q2/baseline_model.py
+++ EX1/Q2/baseline_model.py
@@ -7,9 +7,11 @@ class Baseline_Network(nn.Module):
         self.input_layer = nn.Linear(D_in, H)
         self.output_layer = nn.Linear(H, D_out)
 
-    def random_init(self):
-        nn.init.normal_(self.input_layer.weight, mean=0.0, std=1.0)
-        nn.init.normal_(self.output_layer.weight, mean=0.0, std=1.0)
+    def random_init(self, std=1.0):
+        nn.init.normal_(self.input_layer.weight, mean=0.0, std=std)
+        nn.init.normal_(self.input_layer.bias, mean=0.0, std=std)
+        nn.init.normal_(self.output_layer.weight, mean=0.0, std=std)
+        nn.init.normal_(self.output_layer.bias, mean=0.0, std=std)
 
     def forward(self, x):
         hidden_activation = F.relu(self.input_layer(x))
--- EX1/Q2/train_baseline_model.py
+++ EX1/Q2/train_baseline_model.py
@@ -7,10 +7,8 @@ import numpy as np
 from statistics.calc_statistics import calc_dataset_acc, calc_dataset_loss
 
 if __name__ == "__main__":
-
-    # batch_size is batch size; D_in is input dimension;
-    # H is hidden dimension; D_out is output dimension.
-    epochs = 5
+    # D_in is input dimension; H is hidden dimension; D_out is output dimension.
+    epochs = 300
     batch_size, D_in, H, D_out = 64, 3072, 256, 10
     learning_rate, momentum = 1e-4, 0.9
 
@@ -19,46 +17,58 @@ if __name__ == "__main__":
 
     # Define NN model
     net = Baseline_Network(D_in, H, D_out)
-    net.random_init()
 
     # Define Loss function
     loss_fn = torch.nn.CrossEntropyLoss()
 
-    # Define Optimizer
-    optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+    # preform grid search
+    stds = [0.7, 1.0, 1.3]
+    learning_rates = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+    momentums = [0.5, 0.9, 0.98]
 
-    train_loss_per_epoch, test_loss_per_epoch, train_acc_per_epoch, test_acc_per_epoch = [], [], [], []
+    for i, std in enumerate(stds):
+        for j, learning_rate in enumerate(learning_rates):
+            for k, momentum in enumerate(momentums):
+                # random initialize net
+                net.random_init(std=std)
 
-    # Train Network
-    for epoch in range(epochs):
+                # Define Optimizer
+                optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
 
-        running_loss = 0.0
+                train_loss_per_epoch, test_loss_per_epoch, train_acc_per_epoch, test_acc_per_epoch = [], [], [], []
 
-        for i_batch, sample_batched in enumerate(train_dataloader):
-            images, labels = sample_batched
+                # Train Network
+                for epoch in range(epochs):
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+                    running_loss = 0.0
 
-            # forward + backward + optimize
-            outputs = net(images.float())
-            loss = loss_fn(outputs, labels)
-            loss.backward()
-            optimizer.step()
+                    for i_batch, sample_batched in enumerate(train_dataloader):
+                        images, labels = sample_batched
 
-        # calculate train & test loss
-        epoch_train_loss = calc_dataset_loss(train_dataloader, net, loss_fn)
-        epoch_test_loss = calc_dataset_loss(test_dataloader, net, loss_fn)
-        # calculate train & test accuracy
-        epoch_train_acc = calc_dataset_acc(train_dataloader, net)
-        epoch_test_acc = calc_dataset_acc(test_dataloader, net)
+                        # zero the parameter gradients
+                        optimizer.zero_grad()
 
-        print('[%d]\ttrain_loss: %.3f\t test_loss: %.3f\ttrain_acc: %.3f\ttest_acc: %.3f' %
-              (epoch + 1, epoch_train_loss, epoch_test_loss, epoch_train_acc, epoch_test_acc))
+                        # forward + backward + optimize
+                        outputs = net(images.float())
+                        loss = loss_fn(outputs, labels)
+                        loss.backward()
+                        optimizer.step()
 
-        train_loss_per_epoch.append(epoch_train_loss)
-        test_loss_per_epoch.append(epoch_test_loss)
-        train_acc_per_epoch.append(epoch_train_acc)
-        test_acc_per_epoch.append(epoch_test_acc)
+                    # calculate train & test loss
+                    epoch_train_loss = calc_dataset_loss(train_dataloader, net, loss_fn)
+                    epoch_test_loss = calc_dataset_loss(test_dataloader, net, loss_fn)
+                    # calculate train & test accuracy
+                    epoch_train_acc = calc_dataset_acc(train_dataloader, net)
+                    epoch_test_acc = calc_dataset_acc(test_dataloader, net)
 
-    np.save("curr_stats", np.array([train_loss_per_epoch, test_loss_per_epoch, train_acc_per_epoch, test_acc_per_epoch]))
+                    # if epoch % 10 == 0:
+                    print('[%d_%d_%d]:\t[epoch %d]\ttrain_loss: %.3f\t test_loss: %.3f\ttrain_acc: %.3f\ttest_acc: %.3f' %
+                          (i, j, k, epoch + 1, epoch_train_loss, epoch_test_loss, epoch_train_acc, epoch_test_acc))
+
+                    train_loss_per_epoch.append(epoch_train_loss)
+                    test_loss_per_epoch.append(epoch_test_loss)
+                    train_acc_per_epoch.append(epoch_train_acc)
+                    test_acc_per_epoch.append(epoch_test_acc)
+
+                np.save("statistics/curr_stats_%d_%d_%d" % (i, j, k), np.array([train_loss_per_epoch, test_loss_per_epoch, train_acc_per_epoch, test_acc_per_epoch]))
+            print()
