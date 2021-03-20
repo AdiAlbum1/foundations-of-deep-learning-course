1ae25d4243d19e1a119a2bd5737dc3d23df036b5
Adi <adi.album1@gmail.com>
2021-03-20 16:39:35 +0200

    fix typo

--- EX1/Q2/train_baseline_model.py
+++ EX1/Q2/train_baseline_model.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     # Define Loss function
     loss_fn = torch.nn.CrossEntropyLoss()
 
-    # preform grid search
+    # perform grid search
     stds = [0.7, 1.0, 1.3]
     learning_rates = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
     momentums = [0.5, 0.9, 0.98]
