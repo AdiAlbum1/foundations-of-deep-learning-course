8e44e2f22a69086e2841d84ceef3d36a9bd511a3
Adi <adi.album1@gmail.com>
2021-03-20 16:31:38 +0200

    visualize statistics taking grid-search into consideration

--- EX1/Q2/statistics/visualize_statistics.py
+++ EX1/Q2/statistics/visualize_statistics.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize_loss(train_loss, test_loss):
+def visualize_loss(train_loss, test_loss, i, j, k, std, learning_rate, momentum):
     epochs = len(train_loss)
     x_axis = list(range(1, epochs+1))
 
@@ -9,14 +9,14 @@ def visualize_loss(train_loss, test_loss):
     ax.plot(x_axis, train_loss)
     ax.plot(x_axis, test_loss)
     ax.set(xlabel='epochs', ylabel='loss',
-           title="loss monitor")
+           title="loss\nstd=%.2f, learning_rate=%.2f, momentum=%.2f" %(std, learning_rate, momentum))
     ax.grid()
     plt.legend(["train_loss", "test_loss"])
 
-    fig.savefig("statistics/loss.png")
-    plt.show()
+    fig.savefig("statistics/results/loss_%d_%d_%d.png" %(i, j, k))
+    # plt.show()
 
-def visualize_acc(train_acc, test_acc):
+def visualize_acc(train_acc, test_acc, i, j, k, std, learning_rate, momentum):
     epochs = len(train_acc)
     x_axis = list(range(1, epochs+1))
 
@@ -24,18 +24,25 @@ def visualize_acc(train_acc, test_acc):
     ax.plot(x_axis, train_acc)
     ax.plot(x_axis, test_acc)
     ax.set(xlabel='epochs', ylabel='acc',
-           title="accuracy monitor")
+           title="accuracy\nstd=%.2f, learning_rate=%.2f, momentum=%.2f" %(std, learning_rate, momentum))
     ax.grid()
     plt.legend(["train_acc", "test_acc"])
 
-    fig.savefig("statistics/acc.png")
-    plt.show()
+    fig.savefig("statistics/results/acc_%d_%d_%d.png" %(i, j, k))
+    # plt.show()
 
 if __name__ == "__main__":
-    train_stats = np.load("statistics/curr_stats.npy")
+    stds = [0.7, 1.0, 1.3]
+    learning_rates = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+    momentums = [0.5, 0.9, 0.98]
 
-    train_loss_per_epoch, test_loss_per_epoch, train_acc_per_epoch, test_acc_per_epoch = train_stats
+    for i, std in enumerate(stds):
+        for j, learning_rate in enumerate(learning_rates):
+            for k, momentum in enumerate(momentums):
+                train_stats = np.load("statistics/curr_stats_%d_%d_%d.npy" % (i, j, k))
 
-    visualize_loss(train_loss_per_epoch, test_loss_per_epoch)
-    visualize_acc(train_acc_per_epoch, test_acc_per_epoch)
+                train_loss_per_epoch, test_loss_per_epoch, train_acc_per_epoch, test_acc_per_epoch = train_stats
+
+                visualize_loss(train_loss_per_epoch, test_loss_per_epoch, i, j, k, std, learning_rate, momentum)
+                visualize_acc(train_acc_per_epoch, test_acc_per_epoch, i, j, k, std, learning_rate, momentum)
 
