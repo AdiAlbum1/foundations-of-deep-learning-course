6e7daced170f6384e123b21a2bcf6c8ab8632638
Adi <adi.album1@gmail.com>
2021-03-27 17:25:00 +0300

    dropout option added

--- EX1/Q2/baseline_model.py
+++ EX1/Q2/baseline_model.py
@@ -2,10 +2,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Baseline_Network(nn.Module):
-    def __init__(self, D_in, H, D_out):
+    def __init__(self, D_in, H, D_out, dropout=False, p_dropout=0):
         super(Baseline_Network, self).__init__()
         self.input_layer = nn.Linear(D_in, H)
         self.output_layer = nn.Linear(H, D_out)
+        self.is_dropout = dropout
+        if self.is_dropout:
+            self.dropout = nn.Dropout(p_dropout)
+        self.p_dropout = p_dropout
 
     def normal_random_init(self, std=1.0):
         nn.init.normal_(self.input_layer.weight, mean=0.0, std=std)
@@ -21,5 +25,7 @@ class Baseline_Network(nn.Module):
 
     def forward(self, x):
         hidden_activation = F.relu(self.input_layer(x))
+        if self.dropout:
+            hidden_activation = self.dropout(hidden_activation)
         output = self.output_layer(hidden_activation)
         return output
