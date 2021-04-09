2b8f40261ad5b9fc70e87e9bffac1ae31cb1e2b2
abc <abc@example.com>
2021-04-09 15:53:32 +0300

    current data extractor: '

--- EX1/Q3/dataset_extractor.py
+++ EX1/Q3/dataset_extractor.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import random
 import torch
-
+from sklearn.decomposition import PCA
 from PIL import Image
 
 DATASET_BATCHES = 5
@@ -52,6 +52,13 @@ def normalize_dataset(images):
     # images = [image / 255.0 for image in images]
     return images
 
+def perform_data_whitening(train_data, test_data):
+    pca = PCA(whiten=True)
+    train_data = pca.fit_transform(train_data)
+    test_data = pca.transform(test_data)
+
+    return train_data, test_data
+
 def reorganize_images_shape(images):
     # TO RGB format
     reorganized_images = []
@@ -68,7 +75,7 @@ def reorganize_images_shape(images):
 
     return reorganized_images
 
-def load_dataset(batch_size):
+def load_dataset(batch_size, whiten=False):
     # Unpickle and merge whole CIFAR-10 dataset
     all_train_images, all_train_labels = unpickle_and_merge_train_dataset()
 
@@ -79,13 +86,18 @@ def load_dataset(batch_size):
     train_images, train_labels = subsample_dataset(all_train_images, all_train_labels)
     test_images, test_labels = subsample_dataset(all_test_images, all_test_labels)
 
-    # Reorganize images to RGB format
-    train_images = reorganize_images_shape(train_images)
-    test_images = reorganize_images_shape(test_images)
+    if not whiten:
+      # Reorganize images to RGB format
+      train_images = reorganize_images_shape(train_images)
+      test_images = reorganize_images_shape(test_images)
+    print(whiten)
 
     # Normalize images to [0,1] range by dividing by 255.0
     train_images = normalize_dataset(train_images)
     test_images = normalize_dataset(test_images)
+
+    if whiten:
+      train_images, test_images = perform_data_whitening(train_images, test_images)
 
     # Organize data in PyTorch DataLoader
     tensor_train_x, tensor_train_y = torch.tensor(train_images), torch.tensor(train_labels)
