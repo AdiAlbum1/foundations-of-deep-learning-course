3608270042239128352c88ff1104de76f357326f
Adi <adi.album1@gmail.com>
2021-04-10 11:50:21 +0300

    calculate statistics

--- EX1/Q4/statistics/calc_statistics.py
+++ EX1/Q4/statistics/calc_statistics.py
@@ -1,8 +1,15 @@
-def calc_dataset_loss(dataset_loader, net, loss_fn):
+import torch
+import numpy as np
+
+def calc_dataset_loss(dataset_loader, net, loss_fn, history, batch_size):
     running_loss = 0.0
     for i_batch, sample_batched in enumerate(dataset_loader):
-        images, labels = sample_batched
-        outputs = net(images.float())
+        samples, labels = sample_batched
+        samples = np.reshape(samples, (history, batch_size, 1))
+
+        net.hidden_cell = (torch.zeros(1, 1, net.hidden_layer_size).double(),
+                            torch.zeros(1, 1, net.hidden_layer_size).double())
+        outputs = net(samples.double())
         loss = loss_fn(outputs, labels)
         running_loss += loss.item()
 
