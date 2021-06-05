845b778101f5d01b57913860e41baabf0b97c2d8
Adi <adi.album1@gmail.com>
2021-06-05 17:04:37 +0300

    training procedure

--- EX3/Part 2(4)/statistics/visualize_weights_gradient_magnitude.py
+++ EX3/Part 2(4)/statistics/visualize_weights_gradient_magnitude.py
@@ -2,15 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 if __name__ == "__main__":
-    neural_net_depths = [2, 3, 4]
-    for N in neural_net_depths:
-        weights_gradient_magnitude = np.load("statistics/results/depth_%d/train_weights_gradient_magnitude.npy" %N)
+    weights_gradient_magnitude_N_2 = np.load("statistics/results/depth_2/train_weights_gradient_magnitude.npy")
+    weights_gradient_magnitude_N_3 = np.load("statistics/results/depth_3/train_weights_gradient_magnitude.npy")
+    weights_gradient_magnitude_N_4 = np.load("statistics/results/depth_4/train_weights_gradient_magnitude.npy")
 
-        x_axis = list(range(5, len(weights_gradient_magnitude)))
-        fig, ax = plt.subplots()
-        ax.plot(x_axis, weights_gradient_magnitude[5:])
-        ax.set(xlabel='epochs', ylabel='magnitude', title='Weights\' Gradient Magnitudes\nDepth: %d'%N)
-        ax.grid()
+    x_axis = list(range(len(weights_gradient_magnitude_N_2)))
+    fig, ax = plt.subplots()
+    ax.plot(x_axis, weights_gradient_magnitude_N_2)
+    ax.plot(x_axis, weights_gradient_magnitude_N_3)
+    ax.plot(x_axis, weights_gradient_magnitude_N_4)
+    ax.set(xlabel='epochs', ylabel='magnitude', title='Weights\' Gradient Magnitudes')
+    ax.grid()
+    plt.legend(["N=2", "N=3", "N=4"])
 
-        fig.savefig("statistics/results/depth_%d/weights_gradient_magnitude.png" %N)
-        plt.close(fig)
+    fig.savefig("statistics/results/weights_gradient_magnitude.png")
+    plt.close(fig)
